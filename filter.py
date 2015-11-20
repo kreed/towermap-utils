aa680853456c83fd4b1ed45d3839764f47cca80b
Christopher Eby <kreed@kreed.org>
2015-11-19 20:52:55 -0600

    make filter.py filter on individual bands

--- filter.py
+++ filter.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 import csv
-import os
 import sys
 from pprint import pprint
 
-addr = []
+cell_bands = {}
 
 with open('tmo.csv') as infile:
 	reader = csv.DictReader(infile)
 	for row in reader:
 		if row['id']:
-			addr.append(',' + row['id'] + ',')
+			cell_bands[row['id']] = row['band'].split(';')
 
-os.system('egrep -v "(' + '|'.join(addr) + ')" ' + sys.argv[1])
+with open(sys.argv[1]) as infile:
+	reader = csv.DictReader(infile)
+	writer = csv.DictWriter(sys.stdout, reader.fieldnames)
+	writer.writeheader()
+	for row in reader:
+		pci = row['pci']
+		if pci in cell_bands:
+			if row['band2'] == 'Y' and '2' in cell_bands[pci]:
+				continue
+			if row['band4'] == 'Y' and '4' in cell_bands[pci]:
+				continue
+			if row['band12'] == 'Y' and '12' in cell_bands[pci]:
+				continue
+		writer.writerow(row)
