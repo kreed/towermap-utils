54107d6689ce607a23cba8a1c4d2197ef5134220
Christopher Eby <kreed@kreed.org>
2015-11-21 03:31:46 -0600

    add cancellation date

--- microwave/run.py
+++ microwave/run.py
@@ -18,17 +18,17 @@ cur = con.cursor()
 
 license_locs = {}
 
-q = ("SELECT unique_system_identifier, grant_date, location_class_code, "
+q = ("SELECT unique_system_identifier, grant_date, cancellation_date, location_class_code, "
 	"(lat_degrees+lat_minutes/60.0+lat_seconds/3600.0)*(CASE WHEN lat_direction='S' THEN -1 ELSE 1 END) AS lat, "
 	"(long_degrees+long_minutes/60.0+long_seconds/3600.0)*(CASE WHEN long_direction='W' THEN -1 ELSE 1 END) AS lon "
 	"FROM EN JOIN HD USING (unique_system_identifier) JOIN LO USING (unique_system_identifier) WHERE licensee_id='L00127664' AND "
 	"lon>? AND lat>? AND lon<? AND lat<?")
 q = cur.execute(q, bbox)
 for row in q.fetchall():
-	uls_no, grant_date, class_code, lat, lon = row
+	uls_no, grant_date, cancel_date, class_code, lat, lon = row
 
 	if not uls_no in license_locs:
-		license_locs[uls_no] = [],[],grant_date
+		license_locs[uls_no] = [],[],grant_date,cancel_date
 
 	if class_code == 'T':
 		license_locs[uls_no][0].append((lon,lat))
@@ -40,12 +40,12 @@ for row in q.fetchall():
 result = []
 
 for uls_no, v in license_locs.items():
-	transmitters, receivers, grant_date = v
+	transmitters, receivers, grant_date, cancel_date = v
 	if len(transmitters) != 1 or len(receivers) < 1:
 		print(uls_no, len(transmitters), 'transmitters', len(receivers), 'receivers')
 		continue
 
-	props = {'microwave_uls': uls_no, 'url': 'http://wireless2.fcc.gov/UlsApp/UlsSearch/license.jsp?licKey=%d' % uls_no, 'grant_date': grant_date}
+	props = {'microwave_uls': uls_no, 'url': 'http://wireless2.fcc.gov/UlsApp/UlsSearch/license.jsp?licKey=%d' % uls_no, 'grant_date': grant_date, 'cancellation_date': cancel_date}
 	for r in receivers:
 		geom = geojson.LineString((transmitters[0], r))
 		result.append(geojson.Feature(properties=props, geometry=geom))
