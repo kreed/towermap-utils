49d83d08f15a20a34de3515cee4da7f0d9ff5530
Christopher Eby <kreed@kreed.org>
2015-12-02 19:37:40 -0600

    change micowave output format to csv; remove matching code
    matching will be handled in MLS scripts

--- microwave/run.py
+++ microwave/run.py
@@ -1,30 +1,18 @@
 #!/usr/bin/env python3
 
-import geojson
 import csv
 import os
 import sqlite3
 from geopy.distance import great_circle
 from pprint import pprint
 
-filename = 'micro.geojson'
+filename = 'micro.csv'
 bbox = [-101.3901,27.4839,-91.6095,30.6285]
 
 con = sqlite3.connect(os.path.dirname(os.path.realpath(__file__)) + "/l_micro.sqlite")
 cur = con.cursor()
 
 license_locs = {}
-mapped_sites = {}
-
-with open('/home/chris/code/mls/tmo.csv') as infile:
-	reader = csv.DictReader(infile)
-	for row in reader:
-		if row['microwave_uls']:
-			for uls in row['microwave_uls'].split(';'):
-				uls = int(uls)
-				if not uls in mapped_sites:
-					mapped_sites[uls] = []
-				mapped_sites[uls].append((float(row['lon']), float(row['lat'])))
 
 q = ("SELECT unique_system_identifier, grant_date, cancellation_date, location_class_code, "
 	"(lat_degrees+lat_minutes/60.0+lat_seconds/3600.0)*(CASE WHEN lat_direction='S' THEN -1 ELSE 1 END) AS lat, "
@@ -45,43 +33,16 @@ for row in q.fetchall():
 	else:
 		print('unknown class code', class_code)
 
-result = []
+with open(filename, 'w') as f:
+	print('microwave_uls,website,grant_date,cancellation_date,coords', file=f)
+	for uls_no, v in license_locs.items():
+		transmitters, receivers, grant_date, cancel_date = v
+		if len(transmitters) != 1 or len(receivers) < 1:
+			print(uls_no, len(transmitters), 'transmitters', len(receivers), 'receivers')
+			continue
 
-def match_coord(needle, haystack, max_dist):
-	for coord in haystack:
-		if great_circle(reversed(needle), reversed(coord)).meters < max_dist:
-			return True
-	return False
-
-for uls_no, v in license_locs.items():
-	transmitters, receivers, grant_date, cancel_date = v
-	if len(transmitters) != 1 or len(receivers) < 1:
-		print(uls_no, len(transmitters), 'transmitters', len(receivers), 'receivers')
-		continue
-
-	props = {
-		'microwave_uls': uls_no,
-		'url': 'http://wireless2.fcc.gov/UlsApp/UlsSearch/license.jsp?licKey=%d' % uls_no,
-		'grant_date': grant_date,
-	}
-	if cancel_date:
-		props['cancellation_date'] = cancel_date
-
-	if uls_no in mapped_sites:
-		match = True
-		for coord in (transmitters + receivers):
-			match = match_coord(coord, mapped_sites[uls_no], 1500)
-			if not match:
-				break
-		props['matched'] = match
-
-	for r in receivers:
-		geom = geojson.LineString((transmitters[0], r))
-		result.append(geojson.Feature(properties=props, geometry=geom))
-
-result = geojson.FeatureCollection(result)
-with open(filename, 'w') as out:
-	geojson.dump(result, out)
+		coords = '|'.join('%f|%f' % (x,y) for x,y in (transmitters + receivers))
+		print(uls_no, 'http://wireless2.fcc.gov/UlsApp/UlsSearch/license.jsp?licKey=%d' % uls_no, grant_date, cancel_date, coords, sep=',', file=f)
 
 con.commit()
 con.close()
