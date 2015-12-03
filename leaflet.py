14a08e242f4d9785b3d8d4585280e4c8cca97e79
Christopher Eby <kreed@kreed.org>
2015-12-02 19:21:49 -0600

    rename convert.py to leaflet.py and make it only create tmo.js
    tmo.csv is no longer needed by check.py, so don't bother creating it

--- leaflet.py
+++ leaflet.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import csv
 import geojson
 from lxml import etree
 from pprint import pprint
 
-fields = set()
-rows = []
 features = []
 
 osm = etree.ElementTree(file='/home/chris/Dropbox/osm/tmo.osm')
@@ -33,20 +30,7 @@ for n in osm.iterfind('node'):
 	f = geojson.Feature(geometry=point, properties=props)
 	features.append(f)
 
-	row = dict({'lat': lat, 'lon': lon}, **props)
-	fields |= row.keys()
-	rows.append(row)
-
 features = geojson.FeatureCollection(features)
-
-with open('tmo.csv', 'w') as outfile:
-	writer = csv.DictWriter(outfile, fields)
-	writer.writeheader()
-	for row in rows:
-		writer.writerow(row)
-
-with open('tmo.geojson', 'w') as outfile:
-	geojson.dump(features, outfile)
 with open('tmo.js', 'w') as outfile:
 	outfile.write('var towers = ')
 	geojson.dump(features, outfile)
